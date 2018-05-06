be9363824662e3cef2352a65a2599cda04a1a013
Marin Petrunić <marin.petrunic@gmail.com>
2018-05-06 12:58:31 +0200

    dockerized backend server

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:carbon-alpine
+
+# Install yarn and other dependencies via apk
+RUN apk add --update git python make g++ && \
+  rm -rf /tmp/* /var/cache/apk/*
+
+WORKDIR /usr/app
+
+# Install node dependencies - done in a separate step so Docker can cache it.
+COPY package-lock.json .
+COPY package.json .
+
+RUN npm install --from-lockfile
+
+COPY . .
--- runfile.js
+++ runfile.js
@@ -4,4 +4,16 @@ export function clean() {
   run('rm -rf node_modules');
 }
 
+export function build() {
+  run('docker-compose down');
+  run('docker-compose -f docker-compose-cleanup.yml down -v');
+  run('docker-compose build');
+}
+
+export function dev() {
+  run('docker-compose up');
+}
+
 help(clean, 'Removes all build directories and dependencies');
+help(build, 'Builds new docker image');
+help(dev, 'Starts application and all dependent services');
